fb13b2a41dbf16944e61f561c9c05d2b2c1b4c19
olena4reunited <olenaduringintermission@gmail.com>
2024-10-01 19:57:11 +0300

    fix: flake8

--- musician/serializers.py
+++ musician/serializers.py
@@ -17,5 +17,7 @@ class MusicianSerializer(serializers.ModelSerializer):
 
     def validate_age(self, value):
         if value < 14:
-            raise serializers.ValidationError("You must be at least 14 years old.")
+            raise serializers.ValidationError(
+                "You must be at least 14 years old."
+            )
         return value
